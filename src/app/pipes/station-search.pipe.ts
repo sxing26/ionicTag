b562e36569319c678e23a98dd6f68cc895136bdd
sxing26 <69822358+sxing26@users.noreply.github.com>
2022-02-14 10:12:55 +0100

    [NEW] nouvelles méthodes du service API + méthode récupération des coords des stations d'une ligne dans carte2

--- src/app/pipes/station-search.pipe.ts
+++ src/app/pipes/station-search.pipe.ts
@@ -7,9 +7,15 @@ export class StationSearchPipe implements PipeTransform {
 
   transform(items: string[], filter: string): any {
     if (!items || !filter) {
-      return items;
+      return [];
     }
-    return items.filter(item => item.toLowerCase().indexOf(filter) !== -1);
+    const res = items.filter(item => (item.toLowerCase().indexOf(filter) !== -1));
+    console.log(res.length);
+    if (res.length > 20){
+      return [];
+    } else {
+      return res;
+    }
   }
 
 }
